318c5b0ef04730d2ed274722fb2986db60fdeeef
Min Yang (InConsulting Inc.) <v-miyan@microsoft.com>
2018-08-16 13:01:52 -0700

    finished navigation properties

--- EntityFrameworkPractice/Program.cs
+++ EntityFrameworkPractice/Program.cs
@@ -10,6 +10,49 @@ namespace EntityFrameworkPractice
     {
         static void Main(string[] args)
         {
+            var customers = GetCustomers();
+            foreach( Customer customer in customers)
+            {
+                Console.WriteLine(customer.FirstName.ToString());
+            }
+
+            var customerNames = GetCustomerNames();
+            foreach (string customerName in customerNames)
+            {
+                Console.WriteLine(customerName);
+            }
+            Console.ReadLine();
+        }
+
+        static List<Customer> GetCustomers()
+        {
+            using (var db = new AdventureWorksEntities())
+            {
+                return db.Customers.ToList();
+            }
+        }
+
+        static List<string> GetCustomerNames()
+        {
+            using (var db = new AdventureWorksEntities())
+            {
+                var names = new List<string> { };
+                var customers = db.Customers;
+                foreach( Customer customer in customers)
+                {
+                    names.Add($"{customer.FirstName} {customer.LastName}");
+                }
+
+                return names;
+            }
+        }
+        
+        static List<Customer> GetTopNCustomersSortedByLastName(int n)
+        {
+            using (var db = new AdventureWorksEntities())
+            {
+                return db.Customers.OrderBy(customer => customer.LastName).ThenBy(customer => customer.FirstName).Take(n).ToList();
+            }
         }
     }
 }
